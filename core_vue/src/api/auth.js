e7dcaf3b08fc5e078dd4b3e7d88aa19284fddb66
sal <sahmed9@buffalo.edu>
2019-09-04 13:04:07 -0400

    ok mltoolkit

--- core_vue/src/api/auth.js
+++ core_vue/src/api/auth.js
@@ -2,31 +2,31 @@ import session from './session'
 
 export default {
   login (username, password) {
-    return session.post('https://madeatub.buffalo.edu/mltoolki/auth/login/', { username, password })
+    return session.post('https://madeatub.buffalo.edu/mltoolkit/auth/login/', { username, password })
   },
   logout () {
-    return session.post('https://madeatub.buffalo.edu/mltoolki/auth/logout/', {})
+    return session.post('https://madeatub.buffalo.edu/mltoolkit/auth/logout/', {})
   },
   createAccount (username, password1, password2, email) {
-    return session.post('https://madeatub.buffalo.edu/mltoolki/registration/', { username, password1, password2, email })
+    return session.post('https://madeatub.buffalo.edu/mltoolkit/registration/', { username, password1, password2, email })
   },
   changeAccountPassword (password1, password2) {
-    return session.post('https://madeatub.buffalo.edu/mltoolki/auth/password/change/', { password1, password2 })
+    return session.post('https://madeatub.buffalo.edu/mltoolkit/auth/password/change/', { password1, password2 })
   },
   sendAccountPasswordResetEmail (email) {
-    return session.post('https://madeatub.buffalo.edu/mltoolki/auth/password/reset/', { email })
+    return session.post('https://madeatub.buffalo.edu/mltoolkit/auth/password/reset/', { email })
   },
   resetAccountPassword (uid, token, new_password1, new_password2) { // eslint-disable-line camelcase
-    return session.post('https://madeatub.buffalo.edu/mltoolki/auth/password/reset/confirm/', { uid, token, new_password1, new_password2 })
+    return session.post('https://madeatub.buffalo.edu/mltoolkit/auth/password/reset/confirm/', { uid, token, new_password1, new_password2 })
   },
   getAccountDetails () {
-    return session.get('https://madeatub.buffalo.edu/mltoolki/auth/user/')
+    return session.get('https://madeatub.buffalo.edu/mltoolkit/auth/user/')
   },
   updateAccountDetails (data) {
-    return session.patch('https://madeatub.buffalo.edu/mltoolki/auth/user/', data)
+    return session.patch('https://madeatub.buffalo.edu/mltoolkit/auth/user/', data)
   },
   verifyAccountEmail (key) {
-    return session.post('https://madeatub.buffalo.edu/mltoolki/registration/verify-email/', { key })
+    return session.post('https://madeatub.buffalo.edu/mltoolkit/registration/verify-email/', { key })
   }
 }
 // export default {
--- core_vue/src/api/graph.js
+++ core_vue/src/api/graph.js
@@ -2,22 +2,22 @@ import session from './session'
 
 export default {
   getUserGraphs () {
-    return session.get('https://madeatub.buffalo.edu/mltoolki/api/usergraphs/')
+    return session.get('https://madeatub.buffalo.edu/mltoolkit/api/usergraphs/')
   },
   getRootGraphs () {
-    return session.get('https://madeatub.buffalo.edu/mltoolki/api/rootgraphs/')
+    return session.get('https://madeatub.buffalo.edu/mltoolkit/api/rootgraphs/')
   },
   updateCurrentUserGraph (graphId, payload) {
-    return session.put('https://madeatub.buffalo.edu/mltoolki/api/graphs/' + graphId + '/', payload)
+    return session.put('https://madeatub.buffalo.edu/mltoolkit/api/graphs/' + graphId + '/', payload)
   },
   saveCurrentUserGraph (data) {
-    return session.post('https://madeatub.buffalo.edu/mltoolki/api/graphs/', data)
+    return session.post('https://madeatub.buffalo.edu/mltoolkit/api/graphs/', data)
   },
   runGraph (data) {
-    return session.post('https://madeatub.buffalo.edu/mltoolki/api/rungraph/', data)
+    return session.post('https://madeatub.buffalo.edu/mltoolkit/api/rungraph/', data)
   },
   getGraphRuns () {
-    return session.get('https://madeatub.buffalo.edu/mltoolki/api/graphruns/')
+    return session.get('https://madeatub.buffalo.edu/mltoolkit/api/graphruns/')
   }
 }
 // export default {
