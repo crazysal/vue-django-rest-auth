2db450721151c724d607345330ca56411c03e8e0
bhargavaurala <bhargava.urala@gmail.com>
2019-09-06 13:36:41 -0400

    Update chemmlWrapperPreprocessing_ui_prepare.js

--- core_vue/src/api/jsons/chemmlWrapperPreprocessing_ui_prepare.js
+++ core_vue/src/api/jsons/chemmlWrapperPreprocessing_ui_prepare.js
@@ -61,6 +61,66 @@ export default {
         }
       ],
       nodes: []
+    },
+    {
+      name: 'TrainTestSplit',
+      docstring:
+        'A module to split a data matrix into train and test (or validation) samples',
+      inputs: [
+        {
+          name: 'test_fraction',
+          docstring:
+            'The fraction of samples to be selected for the test. If a value not in (0, 1.0) is provided we default to 0.25',
+          param_type: ['float'],
+          expected_shape: null,
+          is_optional: true,
+          default_value: '0.25',
+          options: null
+        }
+      ],
+      outputs: [],
+      node_functions: [
+        {
+          name: '__init__',
+          docstring: 'None',
+          inputs: [],
+          outputs: []
+        },
+        {
+          name: 'split',
+          display_name: 'train_test_split',
+          docstring:
+            'The function to call for performing the split of the data matrix',
+          inputs: [
+            {
+              name: 'X',
+              docstring: 'the input array',
+              param_type: ['array'],
+              expected_shape: null,
+              is_optional: false,
+              default_value: null,
+              options: null
+            }
+          ],
+          outputs: [
+            {
+              name: 'X1',
+              docstring:
+                'The training data matrix with ((1 - test_fraction) * n_samples) rows',
+              param_type: ['array'],
+              returned: true
+            },
+            {
+              name: 'X2',
+              docstring:
+                'The testing data matrix with (test_fraction * n_samples) rows',
+              param_type: ['array'],
+              returned: true
+            }
+          ]
+        }
+      ],
+      nodes: []
     }
   ],
   library: 'chemml',
